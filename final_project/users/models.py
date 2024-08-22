408a55d65c961cebacbf05bd7d41b4a87580e46b
jposluszny <jposluszny@op.pl>
2024-08-22 19:11:29 +0200

    A maximum length was set for the users' address field in the final project

--- final_project/users/models.py
+++ final_project/users/models.py
@@ -6,7 +6,7 @@ from django.urls import reverse
 class User(AbstractUser):
     staff_member = models.BooleanField(default=False, blank=True)
     born = models.DateField(null=True, blank=True)
-    address = models.TextField()
+    address = models.TextField(max_length=511)
     profile_picture = models.ImageField(default='default.jpeg', upload_to='profile_pictures')
     telefon_number = models.CharField(max_length=9)
     email = models.EmailField(unique=True)
