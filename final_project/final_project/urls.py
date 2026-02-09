ea3a880f0b1be55b0446e80475a3685201117044
jposluszny <jposluszny@op.pl>
2026-02-09 14:12:54 +0100

    Final fix: Change Settings

--- final_project/final_project/urls.py
+++ final_project/final_project/urls.py
@@ -27,10 +27,12 @@ urlpatterns = [
     path('users/', include('users.urls'))
 ]
 
-urlpatterns = [
+urlpatterns += [
     re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
-    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
 
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
